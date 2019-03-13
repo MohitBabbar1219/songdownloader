cb3b41f0534ec8d43cefbff43ffee164dad45ad0
MohitBabbar1219 <mohit@mavenhive.in>
2019-03-13 13:04:17 +0530

    Refactor code to list five options of the song for better judgement of the version to download

--- SliderDownloader.py
+++ SliderDownloader.py
@@ -10,9 +10,13 @@ def get_song_data(song_name):
     return loads(song_json_data.text)
 
 
-def extract_first_song(song_parsed_data):
-    index_cover = list(song_parsed_data['audios'].keys())[0]
-    return song_parsed_data['audios'][index_cover][0]
+def extract_first_five_songs(song_parsed_data):
+    data_of_five_songs = []
+    song_data_keys = list(song_parsed_data['audios'].keys())
+    index_cover = song_data_keys[0]
+    for song_index in range(5):
+        data_of_five_songs.append(song_parsed_data['audios'][index_cover][song_index])
+    return data_of_five_songs
 
 
 def generate_url(song_to_be_downloaded):
@@ -23,17 +27,29 @@ def generate_url(song_to_be_downloaded):
         f"{quote(song_to_be_downloaded['tit_art'])}.mp3?extra={song_to_be_downloaded['extra']}"
 
 
-def get_download_link_and_title_from_name(song_name):
+def get_download_link_and_title_for_first_five_songs_from_name(song_name):
     song_data = get_song_data(song_name)
-    first_song = extract_first_song(song_data)
-    song_title = first_song['tit_art']
-    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
-    song_title = ''.join(c for c in song_title if c in valid_chars)
-    return {'title': song_title, 'url': generate_url(first_song)}
+    five_songs = extract_first_five_songs(song_data)
+    title_and_link_of_songs = []
+    for current_song in five_songs:
+        song_title = current_song['tit_art']
+        duration = current_song['duration']
+        valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+        song_title = ''.join(c for c in song_title if c in valid_chars)
+        title_and_link_of_songs.append({'title': song_title, 'url': generate_url(current_song), 'duration': duration})
+    return title_and_link_of_songs
+
+
+def display_song_options_to_download_from(songs_to_download):
+    for index, song in enumerate(songs_to_download):
+        print(f"{index} -> {song['title']} - {song['duration']}")
+    print("Enter song number to download")
 
 
 def download_song(name):
-    song_to_download = get_download_link_and_title_from_name(name)
+    songs_to_download = get_download_link_and_title_for_first_five_songs_from_name(name)
+    display_song_options_to_download_from(songs_to_download)
+    song_to_download = songs_to_download[int(input())]
     print('Downloading...')
     r = requests.get(song_to_download['url'], allow_redirects=True)
     open(f"{song_to_download['title']}.mp3", 'wb').write(r.content)
